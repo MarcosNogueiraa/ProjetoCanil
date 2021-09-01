b54236e7fce0e8bf26ea3213e1fbd4f93e5cb336
MarcosNogueiraa <marcosogenio@hotmail.com>
2021-09-01 11:15:42 -0300

    Primeiro commit

---
+++ ProjetoCanil/Model/Entidades/Alimentacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoCanil.Model.Entidades
+{
+    class Alimentacao
+    {
+        int IDCachorro; //number(6),
+        string IDAlimento;//varchar2(60),
+        int IDPrescricao; //number(9),
+        float Quantidade; //number(9),
+        string UnidadeMedida; //varchar2(3),
+
+    }
+}
